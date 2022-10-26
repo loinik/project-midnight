7d783776fd2fd762b0fb31d274deea092b56abfa
Michel <loinik@me.com>
2022-10-27 02:39:14 +0600

    TouchNAV fix

--- Ciftree/s2802.js
+++ Ciftree/s2802.js
@@ -6,7 +6,7 @@ function s2802() {
     var locked = false;
     var forwardNAV = AR.Hotspot({
         scene: "s2700",
-        onScreen: [200, 70, 820, 640],
+        onScreen: [200, 70, 824, 640],
         cursor: "Forward",
         active: function() {
             //return not locked
